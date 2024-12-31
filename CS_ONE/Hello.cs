18e3150803142cb7e409672c58606cf7fb5afc36
staticius <ryunaes@gmail.com>
2025-01-01 00:44:48 +0300

    update all

--- CS_ONE/Hello.cs
+++ CS_ONE/Hello.cs
@@ -4,11 +4,55 @@ public class Hello
 {
     public static void Main(string[] args)
     {
+        int ayNumber;
         
-        DateTime dateTime = DateTime.Now;
+        Console.WriteLine("1 - 12 arası bir sayı giriniz: ");
 
-        Console.WriteLine("Hello World!, Date: {0}", dateTime);
+        ayNumber = Convert.ToInt32(Console.ReadLine());
 
-    }
-            
+            switch (ayNumber)
+            {
+                case 1:
+                    Console.WriteLine("{0} Ocak Ayı", ayNumber);
+                    break;
+                case 2:
+                    Console.WriteLine("{0} Şubat Ayı", ayNumber);
+                    break;
+                case 3:
+                    Console.WriteLine("{0} Mart Ayı", ayNumber);
+                    break;
+                case 4:
+                    Console.WriteLine("{0} Nisan Ayı", ayNumber);
+                    break;
+                case 5:
+                    Console.WriteLine("{0} Mayıs Ayı", ayNumber);
+                    break;
+                case 6:
+                    Console.WriteLine("{0} Haziran Ayı", ayNumber);
+                    break;
+                case 7:
+                    Console.WriteLine("{0} Temmuz Ayı", ayNumber);
+                    break;
+                case 8:
+                    Console.WriteLine("{0} Ağustos Ayı", ayNumber);
+                    break;
+                case 9:
+                    Console.WriteLine("{0} Eylül Ayı", ayNumber);
+                    break;
+                case 10:
+                    Console.WriteLine("{0} Ekim Ayı", ayNumber);
+                    break;
+                case 11:
+                    Console.WriteLine("{0} Kasım Ayı", ayNumber);
+                    break;
+                case 12:
+                    Console.WriteLine("{0} Aralık Ayı", ayNumber);
+                    break;
+                default:
+                    Console.WriteLine("1 ve 12 arasında bir sayı girdiğinden emin ol, senin girdiğin sayı: {0}", ayNumber);
+                    break;
+            }
+        }
+
+        
     }
--- CS_ONE/obj/Debug/net9.0/CS_ONE.AssemblyInfo.cs
+++ CS_ONE/obj/Debug/net9.0/CS_ONE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_ONE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c69de32cbbb110835e8db330542073eca68e161")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_ONE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_ONE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
