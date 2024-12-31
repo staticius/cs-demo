7c69de32cbbb110835e8db330542073eca68e161
Staticius <ardadmrkn00@gmail.com>
2024-12-31 17:33:58 +0300

    hell

---
+++ CS_ONE/Hello.cs
@@ -0,0 +1,14 @@
+namespace CS_ONE;
+
+public class Hello
+{
+    public static void Main(string[] args)
+    {
+        
+        DateTime dateTime = DateTime.Now;
+
+        Console.WriteLine("Hello World!, Date: {0}", dateTime);
+
+    }
+            
+    }
